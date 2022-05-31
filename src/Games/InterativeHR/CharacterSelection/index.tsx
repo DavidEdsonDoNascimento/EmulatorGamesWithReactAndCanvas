f2f16de9050a53c1fc77eabf9df1bc2eaa940d8e
DavidEdsonDoNascimento <david.contato.tec@gmail.com>
2022-05-31 16:04:04 -0300

    Sprites of morena-branca.png

--- src/Games/InterativeHR/CharacterSelection/index.tsx
+++ src/Games/InterativeHR/CharacterSelection/index.tsx
@@ -9,13 +9,13 @@ export const CharacterSelection = ({ characterSelectHandler }: CharacterSelectio
         <div className="container">
             <section className="container-selection">
                 <h1>Selecione seu personagem para jogar:</h1>
-                <div className="characterSelect" data-character="chunli" onClick={() => characterSelectHandler('sprites-morena-cabelo-castanho.png')}>
+                <div className="characterSelect" data-character="chunli" onClick={() => characterSelectHandler('sprites-morena-branca.png')}>
                     <img src="img/select_screeen/morena-branca-cabelo-castanho.png" alt="personagem" />
                 </div>
                 <div className="characterSelect" data-character="chunli" onClick={() => characterSelectHandler('sprites-morena-cabelo-castanho.png')}>
                     <img src="img/select_screeen/morena-cabelo-castanho.png" alt="personagem" />
                 </div>
-                <div className="characterSelect" data-character="chunli" onClick={() => characterSelectHandler('sprites-morena-branca.png')}>
+                <div className="characterSelect" data-character="chunli" onClick={() => characterSelectHandler('sprites-mulher-branca.png')}>
                     <img src="img/select_screeen/mulher-branca-cabelo-castanho.png" alt="personagem" />
                 </div>
                 <div className="characterSelect" data-character="chunli" onClick={() => characterSelectHandler(3)}>
